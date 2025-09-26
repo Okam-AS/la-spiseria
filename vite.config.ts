f32f7a03dd848053131b174da62418fe264e8805
asharghi <a.sharghi.mail@gmail.com>
2025-09-26 13:03:15 +0200

    Fix GitHub Pages assets with conditional base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/la-spiseria/",
+  base: mode === 'production' ? "/la-spiseria/" : "/",
   server: {
     host: "::",
     port: 8080,
